5a8bba611b15744f63835074e5dd9495baaa0efc
Elizabeth2412 <163017916+Elizabeth2412@users.noreply.github.com>
2025-10-31 22:29:16 -0500

    Creación de componentes para: cabecera, pie de pagina y login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Header } from './header/header';
+import { Footer } from './footer/footer';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Header},
+    { path: 'pie de pagina', component: Footer}
+];
+    
